67e978cf4c182ea278ae3a53c940c3fb2aa5beae
Scott Cosentino <scottc130@gmail.com>
2022-07-17 17:35:02 -0700

    Completed registration, adding testing now

--- auth.js
+++ auth.js
@@ -8,19 +8,33 @@ export async function registerUser(username,userPassword,firstName,lastName,addr
         var params = getDBParams();
         var sqlConn = new SqlHandler(params[0],params[1],params[2],params[3],params[4]);
 
-        var saltRounds = 10;
-        var salt = await bcrypt.genSalt(saltRounds);
-        var passwordHash = await bcrypt.hash(userPassword,salt);
-
-        var sql = `INSERT INTO user_login(username,user_password,active,role)
-        VALUES (?,?,1,'user')`;
-
-        var result = sqlConn.queryReturnWithParams(sql,[username,passwordHash]);
-
-        return 1;
+        var sql = `SELECT COUNT(*) AS userCount FROM user_login WHERE username = ?`;
+        var result = await sqlConn.queryReturnWithParams(sql,[username]);
+        
+        if (result[0][0].userCount == 0){
+            var saltRounds = 10;
+            var salt = await bcrypt.genSalt(saltRounds);
+            var passwordHash = await bcrypt.hash(userPassword,salt);
+    
+            var sql = `INSERT INTO user_login(username,user_password,active,role)
+            VALUES (?,?,1,'user')`;
+    
+            result = await sqlConn.queryReturnWithParams(sql,[username,passwordHash]);
+            var userId = result[0].insertId;
+    
+            var sql = `INSERT INTO user_details
+            VALUES (?,?,?,?,?)`;
+    
+            sqlConn.queryReturnWithParams(sql,[userId,firstName,lastName,address,postalZipCode]);
+    
+            return 1;
+        }else{
+            return -2;
+        }
 
     } catch (err){
-
+        return -1;
+        
     }
 
 
--- index.js
+++ index.js
@@ -10,9 +10,16 @@ app.post("/registration", async(req,res) => {
         const {username, user_password, first_name, last_name, address, postal_zip_code} = req.body;
         var result = await registerUser(username, user_password, first_name, last_name, address, postal_zip_code);
     
-        res.json(result);
+        if (result == 1){
+            res.json("User created successfully!");
+        }else if (result == -2){
+            res.json("User already exists, pick another username!");
+        }else{
+            res.json("Failed to register user!");
+        }
+
     }catch(err){
-        res.json("Failed");
+        res.json("Failed to register user!");
     } 
 });
 
