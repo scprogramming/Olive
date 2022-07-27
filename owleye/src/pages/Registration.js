4723e634922d35fb3301ca068572ea0acb4e3091
Scott Cosentino <scottc130@gmail.com>
2022-07-27 05:02:39 -0700

    Closing off frontend for now, I'm happy with this iteration for the time being

--- owleye/src/pages/Registration.js
+++ owleye/src/pages/Registration.js
@@ -2,9 +2,11 @@ import {useState} from 'react';
 import '../css/registration.css';
 function Login() {
 
-  const [username,setUsername] = useState("");
+  const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
-  const [firstname,setFirstName] = useState("");
+  const [confirmPassword,setConfirmPassword] = useState("");
+  const [firstName,setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [status,setStatus] = useState("");
 
   const login = (event) =>{
@@ -16,9 +18,11 @@ function Login() {
         "Content-type": "application/json"
       },
       body: JSON.stringify({
-        username: username,
+        email: email,
         user_password: password,
-        first_name: firstname
+        confirm_password:confirmPassword,
+        first_name: firstName,
+        last_name: lastName
       })
     })
     .then((response) => response.json())
@@ -34,13 +38,14 @@ function Login() {
     <h1 className="h3 mb-3 font-weight-normal">Register</h1>
 
     <div class = "form-group">
-        <input className="form-control" id="username" placeholder='Email address' type="email" onChange={(e)=>setUsername(e.target.value)} required autoFocus/>
+        <input className="form-control" id="email" placeholder='Email address' type="email" onChange={(e)=>setEmail(e.target.value)} required autoFocus/>
     </div>
 
     <div class = "form-group">
         <input id="password" className = "form-control" type="password" placeholder='Password' onChange={(e)=>setPassword(e.target.value)} required />
-        <input id="password" className = "form-control" type="password" placeholder='Confirm Password' required />
+        <input id="confirmPassword" className = "form-control" type="password" placeholder='Confirm Password' onChange={(e)=>setConfirmPassword(e.target.value)} required />
         <input id="firstname" className = "form-control" type="text" placeholder="First name" onChange={(e)=>setFirstName(e.target.value)}/>
+        <input id="lastname" className = "form-control" type="text" placeholder="Last name" onChange={(e)=>setLastName(e.target.value)}/>
     </div>
 
     <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
