19ab7c74abc0674903860d3b03d057b421be2947
Scott Cosentino <scottc130@gmail.com>
2022-08-14 07:29:34 -0700

    Updated configuration usage in auth and login

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const server = require('./app.js');
 const dotenv = require("dotenv");
+const config = require("../utils/configuration")
 
 dotenv.config();
-
-const appServer = new server.appServer(process.envdatabaseAddress,
+conf = new config.Configuration(process.env.databaseAddress,
     process.env.databasePort,process.env.databaseUser,process.env.databasePassword,process.env.database);
+
+const appServer = new server.appServer(conf);
 
 appServer.app.listen(5000, () => {
     console.log("Server started");
