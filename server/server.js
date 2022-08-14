1b719df2137c4c9877ad83746ca93dc79b2a89b8
Scott Cosentino <scottc130@gmail.com>
2022-08-14 10:54:36 -0700

    Created configuration updates with address and port updates

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const config = require("../utils/configuration")
 
 dotenv.config();
 conf = new config.Configuration(process.env.databaseAddress,
-    process.env.databasePort,process.env.databaseUser,process.env.databasePassword,process.env.database);
+    process.env.databasePort,process.env.databaseUser,process.env.databasePassword,process.env.database,
+    process.env.serverAddress, process.env.serverPort);
 
 const appServer = new server.appServer(conf);
 
-appServer.app.listen(5000, () => {
+appServer.app.listen(process.env.serverPort, () => {
     console.log("Server started");
 });
