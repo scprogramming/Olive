44db4b12576ea9a4691434d1590165eb33f6ce56
Scott Cosentino <scottc130@gmail.com>
2022-10-04 16:05:54 -0700

    Added edit page logic

--- server/authRouter.js
+++ server/authRouter.js
@@ -1,9 +1,6 @@
-const auth = require('../utils/auth.js');
-const sqlHandle = require('../handlers/DbHandler.js');
 const posts = require("../utils/posts");
 const categories = require("../utils/categories");
-const authRouter = require("./authRouter");
-
+const pages = require('../utils/pages');
 
 module.exports.determineRedirectLogin =  async function determineRedirectLogin(page,status,sqlConn,req){
     if (status[0]){
@@ -15,7 +12,7 @@ module.exports.determineRedirectLogin =  async function determineRedirectLogin(p
 
                 return ["../views/pages/dashboard/categories", {
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     cats:result[0]
                 }];
             case "editPost":
@@ -23,7 +20,7 @@ module.exports.determineRedirectLogin =  async function determineRedirectLogin(p
                 let cats = await categories.getAllCategories(sqlConn);
                 return ["../views/pages/dashboard/editPost",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     posts:result[0],
                     categories: cats[0]
                 }];
@@ -31,7 +28,7 @@ module.exports.determineRedirectLogin =  async function determineRedirectLogin(p
                 result = await posts.getAllPosts(sqlConn);
                 return ["../views/pages/dashboard/posts",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     posts:result[0]
                 }];
             case "addPost":
@@ -39,15 +36,32 @@ module.exports.determineRedirectLogin =  async function determineRedirectLogin(p
                 return["../views/pages/dashboard/addPost",{
                     categories:result[0],
                     url:conf.serverAddress,
-                    port:conf.serverPort}
+                    port:conf.apiPort}
                 ];
+            case "pages":
+                result = await pages.getAllPages(sqlConn);
+                return ["../views/pages/dashboard/pages", {
+                    pages:result[0],
+                    url:conf.serverAddress,
+                    port:conf.apiPort
+                }];
+            case "addPage":
+                return ["../views/pages/dashboard/addPage",{
+                    url:conf.serverAddress,
+                    port:conf.apiPort
+                }];
+            case "editPage":
+                return ["../views/pages/dashboard/editPage",{
+                    url:conf.serverAddress,
+                    port:conf.apiPort
+                }];
         }
     }else{
         switch(status[1]){
             case 1:
                 return ["../views/pages/login",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     regEnable:conf.registrationEnabled
                 }];
             case 2:
@@ -55,25 +69,25 @@ module.exports.determineRedirectLogin =  async function determineRedirectLogin(p
             case 3:
                 return ["../views/pages/login",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     regEnable:conf.registrationEnabled
                 }];
             case 4:
                 return ["../views/pages/login",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     regEnable:conf.registrationEnabled
                 }];
             case 5:
                 return ["../views/pages/login",{
                     url:conf.serverAddress,
-                    port:conf.serverPort,
+                    port:conf.apiPort,
                     regEnable:conf.registrationEnabled
                 }];
             case 6:
             return ["../views/pages/login",{
                 url:conf.serverAddress,
-                port:conf.serverPort,
+                port:conf.apiPort,
                 regEnable:conf.registrationEnabled
             }];
         }
